e484152c391ff823487a8e3ac6d3ee308a622e57
Pranav Khurana <khuranapranav2007@gmail.com>
2025-09-24 15:39:12 -0400

    Gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RI3W/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RI3W/ShooterTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.RI3W;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -15,22 +18,29 @@ public class ShooterTest extends LinearOpMode {
     public static double targetVelocity = 350;
     public static double increment = 0.001;         // Change this in dashboard if you want to control speed with dpads
     public PIDController pidController;
+    public static double P, I, D = 0;
+
+    public double currentVelocity, error;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        double velocityW1, velocityW2;
+        pidController = new PIDController(P, I, D);
+        pidController.setIntegrationBounds(-10000000, 10000000);
         DcMotorEx intake = hardwareMap.get(DcMotorEx.class, "intake");
         DcMotorEx wheel1 = hardwareMap.get(DcMotorEx.class, "wheel1");
         DcMotorEx wheel2 = hardwareMap.get(DcMotorEx.class, "wheel2");
+        wheel2.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
         while (opModeIsActive()) {
+            pidController.setPID(P, I, D);
             intake.setPower(intakePower);
 
-            if (gamepad1.dpad_up) {
-                power += increment;
-            }
-            else if (gamepad1.dpad_down) {
-                power -= increment;
-            }
+//            if (gamepad1.dpad_up) {
+//                power += increment;
+//            }
+//            else if (gamepad1.dpad_down) {
+//                power -= increment;
+//            }
 
             if (gamepad1.right_bumper) {
                 intakePower += increment;
@@ -39,16 +49,18 @@ public class ShooterTest extends LinearOpMode {
                 intakePower -= increment;
             }
 
-            velocityW2 = wheel1.getVelocity(AngleUnit.RADIANS) * 96;
+            currentVelocity = wheel1.getVelocity(AngleUnit.RADIANS) * 48;  // mm
+            error = targetVelocity - currentVelocity;
+            power = pidController.calculate(0, error);
+            power = Range.clip(power, -1, 1);
 
 
             intake.setPower(intakePower);
-            wheel1.setPower(-power);
+            wheel1.setPower(power);
             wheel2.setPower(power);
 
             telemetry.addData("Wheel Power: ", power);
             telemetry.addData("Intake Power: ", intakePower);
-            telemetry.addData("Wheel Velocity: ", velocityW2);
             telemetry.update();
 
         }
